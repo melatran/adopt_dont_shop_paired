7fe06b262eaacd4c3dc046419209a21bc31ffa6b
Melanie Tran <melaniexx02@gmail.com>
2020-05-12 15:06:13 -0600

    Created favorite indicator test in models

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,7 +1,38 @@
 require "rails_helper"
 
-RSpec.describe Favorite, type: :model do
-  describe "relationships" do
-    it {should have_many: favorite_pets}
+RSpec.describe Favorite do
+  before :each do
+    @shelter_1 = Shelter.create(
+      name: "Paws For You",
+      address: "1234 W Elf Ave",
+      city: "Denver",
+      state: "Colorado",
+      zip: "90210",
+    )
+
+    @pet_1 = Pet.create(
+      image: 'https://www.petful.com/wp-content/uploads/2014/01/maltese-1.jpg',
+      name: "Lucy",
+      approximate_age: "4",
+      sex: "female",
+      name_of_shelter_where_pet_is_currently_located: "Paws For You",
+      shelter_id: @shelter_1.id
+    )
+
+    @pet_2 = Pet.create(
+      image: 'https://www.petful.com/wp-content/uploads/2014/01/maltese-1.jpg',
+      name: "MoMo",
+      approximate_age: "4",
+      sex: "male",
+      name_of_shelter_where_pet_is_currently_located: "Paws For You",
+      shelter_id: @shelter_1.id
+    )
+  end
+
+  describe "methods" do
+    it ".total_favorites" do
+    favorites = Favorite.new([@pet_1, @pet_2])
+    expect(favorites.total_favorites).to eq(2)
+    end
   end
 end
