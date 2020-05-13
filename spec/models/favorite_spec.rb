bfdb235b628a34cbfa23340a9cdec6e8f94e88a4
Melanie Tran <melaniexx02@gmail.com>
2020-05-13 11:41:53 -0600

    Fixed the favorite increment test

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,44 +1,11 @@
 require "rails_helper"
 
 RSpec.describe Favorite do
-  before :each do
-    @shelter_1 = Shelter.create(
-      name: "Paws For You",
-      address: "1234 W Elf Ave",
-      city: "Denver",
-      state: "Colorado",
-      zip: "90210",
-    )
+  describe "total_count" do
+    it "can calculate total number of favorites" do
+      favorite = Favorite.new({1 => 2, 2 => 3})
 
-    @pet_1 = Pet.create(
-      image: 'https://www.petful.com/wp-content/uploads/2014/01/maltese-1.jpg',
-      name: "Lucy",
-      approximate_age: "4",
-      sex: "female",
-      name_of_shelter_where_pet_is_currently_located: "Paws For You",
-      shelter_id: @shelter_1.id
-    )
-
-    @pet_2 = Pet.create(
-      image: 'https://www.petful.com/wp-content/uploads/2014/01/maltese-1.jpg',
-      name: "MoMo",
-      approximate_age: "4",
-      sex: "male",
-      name_of_shelter_where_pet_is_currently_located: "Paws For You",
-      shelter_id: @shelter_1.id
-    )
-  end
-
-  describe "methods" do
-    it ".total_favorites" do
-    favorites = Favorite.new([@pet_1, @pet_2])
-    expect(favorites.total_favorites).to eq(2)
-    end
-
-    it ".add_pet" do
-      favorites = Favorite.new
-      favorites.add_pet(1)
-      expect(favorites.pets).to eq([1])
+      expect(favorite.total_count).to eq(5)
     end
   end
 end
