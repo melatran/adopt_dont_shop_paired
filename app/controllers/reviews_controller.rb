be12e23f0764c0f333f35400d05cf9e7a60fecaa
Mad Hallé <mad.halle@icloud.com>
2020-05-13 17:31:23 -0600

    User story 4 complete. Reviews controller has flash functionality as well as new review form html

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,37 +1,23 @@
 class ReviewsController < ApplicationController
 
   def new
-    # @shelter = Shelter.find(params[:id])
     @shelter_id = params[:shelter_id]
-
   end
 
   def create
     shelter = Shelter.find(params[:shelter_id])
-    review = shelter.reviews.create!(review_params)
-    redirect_to "/shelters/#{shelter.id}"
+    review = shelter.reviews.new(review_params)
+    if review.save
+      flash[:notice] = "Review successfully created!"
+      redirect_to "/shelters/#{shelter.id}"
+    else
+      flash[:notice] = "Review not created: please fill out all indicated fields"
+      redirect_to "/shelters/#{shelter.id}/new_review"
+    end
   end
-  # def create
-  #
-  #   @review = Review.new(review_params)
-  #   require "pry"; binding.pry
-  #   @shelter_id = @review.shelter_id
-  #   # @review = Review.new(shelter_id: shelter_id[:id])
-  #   # require "pry"; binding.pry
-  #   if @review.save
-  #     redirect_to "/shelters/#{@shelter_id}"
-  #   else
-  #     flash[:notice] = "Failed to enter review information: Please enter all info"
-  #     redirect_to "/shelters/#{@shelter_id}/new_review"
-  #   end
-  # end
 
   private
   def review_params
     params.permit(:title, :rating, :content, :image)
-    # params.require(:review).permit(:title, :rating, :content, :image)
   end
-  # def shelter_id
-  #   params.permit(:id)
-  # end
 end
--- spec/features/reviews/create_spec.rb
+++ spec/features/reviews/create_spec.rb
@@ -1,17 +1,4 @@
 require 'rails_helper'
-# [ ] done
-#
-# User Story 2, Shelter Reviews
-#
-# As a visitor,
-# When I visit a shelter's show page,
-# I see a list of reviews for that shelter
-# Each review will have:
-# - title
-# - rating
-# - content
-# - an optional picture
-# `
 
 RSpec.describe "review creation page" do
   before :each do
@@ -45,31 +32,18 @@ RSpec.describe "review creation page" do
     expect(page).to have_content(2)
     expect(page).to have_content('I have never left a place with so many scratches.')
   end
+
+  it "will display flash message prompting user to fill in all forms" do
+    visit "/shelters/#{@shelter_1.id}"
+
+    click_link "Add review for this shelter"
+    expect(current_path).to eq("/shelters/#{@shelter_1.id}/new_review")
+
+    fill_in :title, with: 'Claws for you'
+    click_on "Submit"
+
+    expect(current_path).to eq("/shelters/#{@shelter_1.id}/new_review")
+    expect(page).to have_content("Review not created: please fill out all indicated fields")
+    expect(current_path).to eq("/shelters/#{@shelter_1.id}/new_review")
+  end
 end
-# [ ] done
-#
-# User Story 3, Shelter Review Creation
-#
-# As a visitor,
-# When I visit a shelter's show page
-# I see a link to add a new review for this shelter.
-# When I click on this link, I am taken to a new review path
-# On this new page, I see a form where I must enter:
-# - title
-# - rating
-# - content
-# I also see a field where I can enter an optional image (web address)
-# When the form is submitted, I should return to that shelter's show page
-# and I can see my new review
-# ```
-#
-# ```
-# [ ] done
-#
-# User Story 4, Shelter Review Creation, cont.
-#
-# As a visitor,
-# When I fail to enter a title, a rating, and/or content in the new shelter review form, but still try to submit the form
-# I see a flash message indicating that I need to fill in a title, rating, and content in order to submit a shelter review
-# And I'm returned to the new form to create a new review
-# ```
